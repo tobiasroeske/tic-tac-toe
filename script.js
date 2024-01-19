6a79ca2a8a115015d6bddecea3a39f8544a2357f
Tobias Roeske <t.roeske91@gmail.com>
2024-01-19 11:39:20 +0100

    Winning line works

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function init() {
 
 function render() {
     const contentDiv = document.getElementById('content');
-
+    contentDiv.innerHTML = '';
     // Generate table HTML
     let tableHtml = '<table>';
     for (let i = 0; i < 3; i++) {
@@ -81,13 +81,15 @@ function drawWinningLine(combination) {
   const lineColor = '#ffffff';
   const lineWidth = 5;
 
-  const startCell = document.querySelectorAll(`td`)[combination[0]];
-  const endCell = document.querySelectorAll(`td`)[combination[2]];
+  const startCell = document.querySelectorAll('td')[combination[0]];
+  const endCell = document.querySelectorAll('td')[combination[2]];
   const startRect = startCell.getBoundingClientRect();
   const endRect = endCell.getBoundingClientRect();
 
+  const contentRect = document.getElementById('content').getBoundingClientRect();
+
   const lineLength = Math.sqrt(
-      Math.pow(endRect.left - startRect.left, 2) + Math.pow(endRect.top - startRect.top, 2)
+    Math.pow(endRect.left - startRect.left, 2) + Math.pow(endRect.top - startRect.top, 2)
   );
   const lineAngle = Math.atan2(endRect.top - startRect.top, endRect.left - startRect.left);
 
@@ -96,11 +98,13 @@ function drawWinningLine(combination) {
   line.style.width = `${lineLength}px`;
   line.style.height = `${lineWidth}px`;
   line.style.backgroundColor = lineColor;
-  line.style.top = `${ startRect.top + startRect.height / 2 - lineWidth / 2 } px`;
-  line.style.left = `${ startRect.left + startRect.width / 2 } px`;
-  line.style.transform = `rotate(${ lineAngle }rad)`;
+  line.style.top = `${startRect.top + startRect.height / 2 - lineWidth / 2 - contentRect.top}px`;
+  line.style.left = `${startRect.left + startRect.width / 2 - contentRect.left}px`;
+  line.style.transform = `rotate(${lineAngle}rad)`;
+  line.style.transformOrigin = 'top left';
   document.getElementById('content').appendChild(line);
 }
+
 
 function generateCircleSVG() {
   const color = '#00B0EF';
@@ -133,8 +137,20 @@ function generateCrossSVG() {
   return svgHtml;
 }
 
- 
-  
-
+function restartGame() {
+  fields = [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null
+  ];
+  currentPlayer = 'circle';
+  render();
+}
   
   
